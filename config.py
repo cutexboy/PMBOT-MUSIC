0ac9285f8efe57c3ae1cccfa1a26ccb38f127b93
Prince kumar <112043341+cutexboy@users.noreply.github.com>
2024-01-27 07:06:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Esprorepo/EsproMusicBot",
+    "https://github.com/cutexboy/PMBOT-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "ritik")
 GIT_TOKEN = getenv(
